27b849cfde60adac9bb266b9a074c91e8201ef23
jpjpaula <jpjpaula2@gmail.com>
2025-11-18 19:53:58 -0300

    Create operacoesmatematicas_test.c

---
+++ operacoesmatematicas_test.c
@@ -0,0 +1,19 @@
+
+#include <assert.h>
+#include <stdio.h>
+
+int multiplicar(int a, int b);
+int dividir(int a, int b);
+
+int main() {
+
+    
+    assert(multiplicar(2, 3) == 6);
+    assert(multiplicar(-4, 5) == -20);
+
+    assert(dividir(10, 2) == 5);
+    assert(dividir(-9, 3) == -3);
+
+    printf("Todos os testes passaram com sucesso!\n");
+    return 0;
+}
